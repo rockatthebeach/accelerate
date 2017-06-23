2ab60b3fc4aefafd424095b22cb988c5e9ff8195
rockatthebeach <rockyjaeger@hotmail.com>
2017-06-23 07:11:55 -0700

    update functions.php and single-case_studies.php files

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -32,7 +32,7 @@ get_header(); ?>
        <h2><?php the_title(); ?></h2>
        <?php the_excerpt(); ?> 
        <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-  <?php endwhile; ?>
+   <?php endwhile; ?>
 <?php wp_reset_query(); ?>
     	</div>
 	</div>
